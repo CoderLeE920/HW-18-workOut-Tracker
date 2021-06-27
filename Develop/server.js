494a787f405b3bac61dcd218e043eb15ec8f7754
CoderLeE920 <72462166+CoderLeE920@users.noreply.github.com>
2021-06-27 07:06:28 -0600

    diamonddog

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require("express");
-const mongoose = require("mongoose");
-require('dotenv').config()
+const mongoose = require("mongoose")
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -24,7 +24,7 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api.js"));
-app.use(require("./routes/htmlRoutes.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
